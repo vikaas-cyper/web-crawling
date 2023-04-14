50432beae7c7bfe3f908e9767e534c974da14a2f
vikaas-a <vikaas.cs20@bitsathy.ac.in>
2023-04-14 13:56:31 +0530

    crawl single page

--- crawl.js
+++ crawl.js
@@ -1,4 +1,30 @@
 const {JSDOM} = require('jsdom')
+
+async function crawlPage(currentURL){
+    console.log(`activity crawling at ${currentURL}`)
+    
+    try{
+        const resp = await fetch(currentURL)
+
+        if (resp.status >399){
+            console.log(`error in fetch with statun code: ${resp.status} on page: ${currentURL}`)
+            return 
+        }
+
+        const contentType = resp.headers.get("content-type")
+        if (!contentType.includes("text/html")){
+            console.log(`\nnon html response, content type: ${contentType}, on page: ${currentURL}`)
+            return 
+        }
+        
+        console.log(await resp.text())    
+    }
+
+    catch(err){
+        console.log(`error in fetch: ${err.message} on page: ${currentURL} ` )
+    }
+    
+}
 function getURLsFromHTML(htmlBody, baseURL){
 
     const urls = []
@@ -45,5 +71,6 @@ function normalizeUrl(urlString){
 
 module.exports = {
     normalizeUrl,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
